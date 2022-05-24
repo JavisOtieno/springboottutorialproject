f727ba09742a199b849d0148d55fe2d8267a0575
jomondi <javisotieno@gmail.cpom>
2022-05-24 08:34:49 +0300

    Springboot udemy

---
+++ src/main/java/com/in28minutes/springbootfirstwebapplication2war/Springbootfirstwebapplication2warApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springbootfirstwebapplication2war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootfirstwebapplication2warApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootfirstwebapplication2warApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/springbootfirstwebapplication2war/Springbootfirstwebapplication2warApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springbootfirstwebapplication2war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootfirstwebapplication2warApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
